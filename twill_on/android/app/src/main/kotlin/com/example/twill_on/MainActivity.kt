e32d49c73c985a9aeb18249f88f79b480b6c0603
Jennifer Mah <JENNIFERMAH98@GMAIL.COM>
2020-12-08 13:44:35 -0700

    first commit

---
+++ twill_on/android/app/src/main/kotlin/com/example/twill_on/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twill_on
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ twill_on/lib/screens/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:twill_on/widgets/category_selector.dart';
+import 'package:twill_on/widgets/recent_chats.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor, //makes background red
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Twill-On',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          //search icon
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      //menu selector/segmented controller
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+          // curved part
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  // FavoriteContacts(),
+                  RecentChats(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
